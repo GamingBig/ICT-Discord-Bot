ea99e0f80cf9fea2bab3aa2561d7605138f1df6f
GamingBig <44531907+GamingBig@users.noreply.github.com>
2021-09-01 13:26:34 +0200

    almost done help, half way at vc

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const env = envFile.parsed
 const config = require('./config.json');
 var Filter = require('bad-words')
 const googleTTS = require('google-tts-api')
+const disbut = require("discord-buttons");
+const disvoice = require('@discordjs/voice');
+
+//help command setup
+var getClosest = require("get-closest");
+const levenshtein = require("levenshtein")
+const helpJSON = require("./help.json")
+var commands = []
+const players = {}
+
+Object.keys(helpJSON).forEach((value, i) => {
+    if (value == "Catagories") {
+        return
+    }
+    Object.keys(helpJSON[value]).forEach((value, i) => {
+        commands.push(value)
+    })
+})
 
 //nieuwe bullshit van discord js v13
 const myIntents = new discord.Intents();
@@ -14,7 +32,30 @@ var client = new discord.Client({ intents: myIntents })
 
 client.on("ready", () => {
     console.log("Logged in as: " + client.user.tag)
+    client.user.setActivity("how long can I procrastinate for.", {
+        type: "PLAYING",
+    });
 })
+
+//leave when other have left
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if (!oldState.guild.voice || !oldState.guild.voice.channel) {
+        return
+    }
+    if (oldState.channel !== null && newState.channel === null) {
+        var memberArray = oldState.guild.me.voice.channel.members
+        var realMemberArray = []
+        memberArray.array().forEach(element => {
+            if (element.user.bot == false) {
+                realMemberArray.push(element.user)
+            }
+        })
+        if (realMemberArray.length == 0) {
+            oldState.guild.voice.channel.leave()
+        }
+    }
+});
+
 
 client.on('guildMemberAdd', (guildMember) => {
     guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name === "Standaard rol"));
@@ -30,8 +71,48 @@ client.on("messageCreate", async (msg) => {
     if (!msg.content.startsWith(curPrefix)) { return }
     var command = msg.content.split(" ")[0].slice(curPrefix.length).toLowerCase()
     var args = msg.content.slice(command.length + curPrefix.length + 1).split(" ")
-    console.log("Request Info:\n    " + msg.content + "\n    " + command + "\n    " + curPrefix + "\n    " + args + "\n")
-    if (command == "prefix") {
+    if (config.debug == true) {
+        console.log("Request Info:\n    " + msg.content + "\n    " + command + "\n    " + curPrefix + "\n    " + args + "\n")
+    }
+    /*start of commands,
+    Help command*/
+    if (command.startsWith("help")) {
+        var arg2 = args[0]
+        if (arg2 == "" || arg2 == undefined) {
+            var embed = new Discord.MessageEmbed()
+                .setColor("#1ecc18")
+                .setTitle('Please select a catagory.')
+            var defButton = new disbut.MessageButton()
+                .setLabel("Catagories")
+                .setStyle('grey')
+                .setID("H-Catagories")
+            var defButtonRow = new disbut.MessageActionRow().addComponent(defButton)
+            var buttonRow = new disbut.MessageActionRow()
+            for (var key in helpJSON.Catagories) {
+                embed.addField(key, helpJSON.Catagories[key])
+                var keyButton = new disbut.MessageButton()
+                    .setID("H-" + key)
+                    .setLabel(key)
+                    .setStyle('blurple')
+                buttonRow.addComponent(keyButton)
+            }
+            msg.channel.send({ embed: embed, components: [buttonRow, defButtonRow] });
+            return
+        }
+        var arg2C = arg2.charAt(0).toUpperCase() + arg2.slice(1);
+        if (!helpJSON[arg2C]) {
+            message.channel.send("Not a valid catagory.")
+            return
+        }
+        var catagory = helpJSON[arg2C]
+        var embed = new Discord.MessageEmbed()
+            .setColor("#1ecc18")
+            .setTitle('All commands for catagory: ' + arg2C)
+        for (var key in catagory) {
+            embed.addField(key, catagory[key])
+        }
+        message.channel.send(embed);
+    } else /*Prefix command*/ if (command == "prefix") {
         if (!args[0]) {
             return msg.channel.send("You did not specify a prefix.\nThe prefix for this server is :`" + curPrefix + "`")
         }
@@ -39,37 +120,74 @@ client.on("messageCreate", async (msg) => {
         fs.writeFileSync("config.json", JSON.stringify(config))
         curPrefix = args[0]
         msg.channel.send("The prefix is now: `" + curPrefix + "`")
-    } else if (command == "say") {
-        say(msg, args, command, curPrefix)
-    } else if (command == "nick" || command == "nick") {
+    } else /*Nickname command*/ if (command == "nick" || command == "nick") {
         if (!args[0]) {
             return msg.channel.send("You did not specify a nickname.")
         }
-        if (!msg.member.nickname.includes("(") && !args.join(" ").includes("(")) {
+        if (!msg.member.nickname) {
+            var name = msg.member.displayName
+        } else {
+            var name = msg.member.nickname
+        }
+        if (!name.includes("(") && !args.join(" ").includes("(")) {
+            if (!name && !args.join(" ").includes("(")) {
+                msg.member.setNickname(args.join(" "))
+                return msg.channel.send("Your nickname is now: `" + args.join(" ") + "`\n\nPlease add your real name to your nickname like this: `XX_SniperKiller_XX (Karel)`.")
+            }
             msg.member.setNickname(args.join(" "))
             msg.channel.send("Your nickname is now: `" + args.join(" ") + "`\n\nPlease add your real name to your nickname like this: `XX_SniperKiller_XX (Karel)`.")
             return
         }
-        var oldName = msg.member.nickname
+        var oldName = name
         var newName = args.join(" ")
         newName = newName.replace(/( \(unde)\w+/g, "")
         await msg.member.setNickname(newName)
         msg.channel.send("Your nickname is now: `" + newName + "`")
-        msg.guild.me.voice
     }
 })
 
+//Voice commands
+client.on("messageCreate", async (msg) => {
+    if (msg.member.user == client.user || !msg.content.startsWith(curPrefix)) { return }
+    var curPrefix = config.prefix[msg.guildId]
+    if (!curPrefix) { config.prefix[msg.guildId] = "!"; fs.writeFileSync("config.json", JSON.stringify(config)); config = require("config.json") }
+    var command = msg.content.split(" ")[0].slice(curPrefix.length).toLowerCase()
+    var args = msg.content.slice(command.length + curPrefix.length + 1).split(" ")
+    if (command.startsWith("join")) {
+        if (!msg.member.voice) {
+            return msg.channel.send("You need to be in a voice channel for me to join.")
+        }
+        msg.member.voice.channel.join().then(connection => {
+            connection.voice.setSelfDeaf(true)
+            connection.voice.setSelfMute(true)
+        })
+    } else if (command.startsWith("leave")) {
+        if (msg.guild.voice == undefined) {
+            return msg.channel.send("I'm not in a voice channel.")
+        }
+        msg.guild.voice.channel.leave()
+    } else /*Say command*/ if (command == "say") {
+        say(msg, args, command, curPrefix)
+    }
+})
+
+
 async function say(msg, args, command, curPrefix) {
+    // get text
     var text = args.join(" ")
-    var Connection
+    //join vc
     if (msg.guild.me.voice.channel) {
-        await msg.guild.me.voice.channel.join().then(connection => {
-            Connection = connection
-        }).catch(err => console.log(err));
+        var connection = disvoice.joinVoiceChannel({
+            channelId: msg.guild.me.voice.channelId,
+            guildId: msg.guildId,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
     } else if (msg.member.voice.channel) {
-        await msg.member.voice.channel.join().then(connection => {
-            Connection = connection
-        }).catch(err => console.log(err));
+        var connection = disvoice.joinVoiceChannel({
+            channelId: msg.member.voice.channelId,
+            guildId: msg.guildId,
+            adapterCreator: msg.guild.voiceAdapterCreator,
+        });
     } else {
         msg.channel.send("I need to be in a voice channel to say stuff, use !join @user to make me join a channel.")
     }
@@ -91,14 +209,21 @@ async function say(msg, args, command, curPrefix) {
     if (oldText !== text) {
         msg.delete()
     }
-    //
+    // new discordjs V13 type
     const url = googleTTS.getAudioUrl(text, {
         lang: TextLanguage,
         slow: false,
         host: 'https://translate.google.com',
     })
-    var dispatcher = connection.play(url)
+    var curPlayer = players[msg.guildId]
+    if (!curPlayer || !curPlayer.player || !curPlayer.res) {
+        curPlayer.player = disvoice.createAudioPlayer()
+        curPlayer.res = disvoice.createAudioResource(url)
+    }
+    curPlayer.player
     dispatcher.setVolume(volume)
 }
+//                  end of say command
+
 
 client.login(env.discord_token)
